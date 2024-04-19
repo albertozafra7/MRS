64ffbf579da1b741604cfc249c2807b2d5a9cf21
Jorg403 <76389552+Jorg403@users.noreply.github.com>
2024-04-20 01:39:03 +0200

    videos yes

--- Part2_Consensus/Lab/lab2.py
+++ Part2_Consensus/Lab/lab2.py
@@ -6,7 +6,6 @@ from bot import bot
 import plotter
 
 
-
 # basic initialization
 N_bots = 5
 N_iterations = 2000
@@ -16,9 +15,8 @@ initial_pos = np.linspace(-5, 5,num=11)
 #print(initial_pos)
 connections = [[0, 1], [0, 2], [1,2], [2, 3], [4,1]]
 # [[0, 1], [0, 2], [1,2], [2, 3]]
-robot_evolution_x = np.zeros(N_bots)
-robot_evolution_y = np.zeros(N_bots)
-
+robot_evolution_x = np.zeros((N_bots, N_iterations+1))
+robot_evolution_y = np.zeros((N_bots, N_iterations+1))
 
 
 # Bots initialization
@@ -28,8 +26,8 @@ for i in range(N_bots):
     pos = ([random.choice(initial_pos),random.choice(initial_pos)])
     bots.append(bot(i,[],pos,update_period,step_size))
     # Store the positions of the robots
-    robot_evolution_x[i] = pos[0]
-    robot_evolution_y[i] = pos[1]
+    robot_evolution_x[i,0] = pos[0]
+    robot_evolution_y[i,0] = pos[1]
 
 for conn in connections:
     bots[conn[0]].add_neighbor(bots[conn[1]])
@@ -37,13 +35,8 @@ for conn in connections:
 
 for it in range(N_iterations):
     # store its evolution
-    if(it>0):
-        x = robot_evolution_x[-1]
-        y = robot_evolution_y[-1]
-    else:
-        x = robot_evolution_x
-        y = robot_evolution_y
-        
+    x = robot_evolution_x[:,it]
+    y = robot_evolution_y[:,it]
         
     for b in bots:
        id1, pos1, id2, pos2 = b.step_rendevous()
@@ -55,12 +48,15 @@ for it in range(N_iterations):
        y[id1] = pos1[1]
        y[id2] = pos2[1]
        
-    robot_evolution_x = np.vstack((robot_evolution_x,x))
-    robot_evolution_y = np.vstack((robot_evolution_y,y))   
-
-
+    robot_evolution_x[:,it+1] = x
+    robot_evolution_y[:,it+1] = y
 
 print(np.shape(robot_evolution_y))
 
+# Plot the results
+# plotter.plot_evolutionxy(robot_evolution_x,robot_evolution_y)
+# plotter.plot_evolutionx(robot_evolution_x)
+# plotter.plot_evolutiony(robot_evolution_y)
 
-
+# Video?
+plotter.plot_evolutionxy_video(robot_evolution_x[:,::10],robot_evolution_y[:,::10])
--- Part2_Consensus/Lab/plotter.py
+++ Part2_Consensus/Lab/plotter.py
@@ -1,42 +1,56 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import random
+from PIL import Image
 import numpy as np  # Make numpy available using np.
 from matplotlib import pyplot as plt # Plotting of the graph
-from matplotlib import animation
+import imageio
+import os
 
+def remove_all_images(folder_path):
+  for filename in os.listdir(folder_path):
+    if os.path.isfile(os.path.join(folder_path, filename)):
+      # Check for image extensions (modify as needed)
+      if filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".jpeg"):
+        os.remove(os.path.join(folder_path, filename))
+    print(f"Deleted images")
 
 def plot_evolutionxy_video(robot_evolution_x, robot_evolution_y):
-    fig, ax = plt.subplots()  # Create a figure and an axes object
+    plt.switch_backend('Agg')
+    # Define plot limits (adjust as needed)
+    x_min, x_max = -10, 10
+    y_min, y_max = -10, 10
+    (n, num_iter)=robot_evolution_x.shape
+    # Function to generate a single plot frame
+    def generate_plot(frame_num):
+        plt.clf()  # Clear previous plot
+        plt.xlim(x_min, x_max)
+        plt.ylim(y_min, y_max)
+        plt.xlabel("X")
+        plt.ylabel("Y")
+        plt.title(f"Robot Positions - Frame {frame_num}")
 
-    # Set plot labels and title
-    ax.set_title("Evolution of the x,y coordinates of each node")
-    ax.set_xlabel("x axis")
-    ax.set_ylabel("y axis")
+        # Plot robots based on their positions at the current frame
+        for i in range(n):
+            x = robot_evolution_x[i,frame_num]
+            y = robot_evolution_y[i,frame_num]
+            plt.plot(x, y, 'o')  # Marker 'o' for circle
 
-    (n, num_iter) = robot_evolution_x.shape  # Get the number of nodes and iterations
+    # Create the GIF animation
+    frames = []
+    for i in range(num_iter):  # Assuming all robots have same number of positions
+        if i%50==0:
+            print(i*100/num_iter,"%")
+        plt.clf()  # Clear previous plot
+        generate_plot(i)
+        filename = f"imgs/frame_{i}.png"
+        plt.savefig(filename)
+        frames.append(imageio.imread(filename))
 
-    # Initialize empty line objects for animation
-    lines = [ax.plot([], [], marker='.')[0] for i in range(n)]  # Create lines for each node
+    imageio.mimsave('robot_movement.gif', frames, duration=20)  # Save the animation as GIF
 
-    # Function to update the animation frame
-    def animate(i):
+    remove_all_images("./imgs")
 
-        for j in range(n):
-            lines[j].set_data(robot_evolution_x[j, :i], robot_evolution_y[j, :i])  # Update line data
-
-        # Special markers for the first and last values (outside the loop for efficiency)
-        ax.plot(robot_evolution_x[0, :i], robot_evolution_y[0, :i], marker='x')
-        ax.plot(robot_evolution_x[-1, :i], robot_evolution_y[-1, :i], marker='o')
-
-        return lines
-
-    # Create animation object
-    anim = animation.FuncAnimation(fig, animate, frames=num_iter, interval=10)  # Adjust interval for animation speed
-    writergif = animation.PillowWriter(fps=30)
-    # Save animation as video (replace 'animation.mp4' with your desired filename)
-    anim.save('animation', writer=writergif)  # You may need to install ffmpeg for this to work
-
-    plt.close(fig)  # Close the plot
+    print("Robot movement GIF created!")
 
 def plot_evolutionxy(robot_evolution_x,robot_evolution_y):
     plt.title("Evolution of the x,y coordinates of each node")
