cb7a4ce76fea37be450a91299dd89a0e1b7cc446
albertozafra7 <albertozafra7@gmail.com>
2024-05-28 18:13:12 +0200

    TODO AL 8

--- Final_project/mrs/Communication.py
+++ Final_project/mrs/Communication.py
@@ -116,7 +116,7 @@ class Communication:
                     # Create a server proxy object
                     with self.comm_lock:
                         print("C: Getting poses from server", HOST, PORT)
-                        new_poses = np.array(self.server.RPC_get_poses(self.poses.tolist()))#self.server[i].RPC_get_poses(self.poses.tolist()))
+                        new_poses = np.array(self.server[i].RPC_get_poses(self.poses.tolist()))
                         print("C: Received new poses:", new_poses)
                         np.copyto(self.poses, new_poses)  # Copy the new data into the existing shared array
                         print("C: Updated poses array:", self.poses)
