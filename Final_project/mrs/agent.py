9d63004c4787a5f1f4ba4649e80eb37241158edd
albertozafra7 <albertozafra7@gmail.com>
2024-05-28 13:38:06 +0200

    Agent printing v2

--- Final_project/mrs/agent.py
+++ Final_project/mrs/agent.py
@@ -86,8 +86,8 @@ class agent:
             self.com.update_position(self.idG, self.idL, self.qL[self.idL,:])
             
             print("A: Agent " + str(self.uid) + " has the following qs")
-            print("qL = " + str(self.qL))
-            print("qG = " + str(self.qG))
+            print("qL = " + str(self.com.get_local_positions(group_id=self.idG)))
+            print("qG = " + str(self.com.get_global_positions()))
 
             # Maybe here a pause is needed???
             time.sleep(0.02)
