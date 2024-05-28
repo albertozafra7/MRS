a7ecae3081cdbccf4075e57408115553fbdb90c3
albertozafra7 <albertozafra7@gmail.com>
2024-05-28 13:50:55 +0200

    Communications ChatGPT correction

--- Final_project/mrs/Communication.py
+++ Final_project/mrs/Communication.py
@@ -113,7 +113,8 @@ class Communication:
                 try:
                     # Create a server proxy object
                     with self.comm_lock:
-                        self.poses = np.array(self.server[i].RPC_get_poses(self.poses.tolist()))
+                        new_poses = np.array(self.server[i].RPC_get_poses(self.poses.tolist()))
+                        np.copyto(self.poses, new_poses)  # Copy the new data into the existing shared array
                 except Exception as e:
                     print("C:","exception: ",e)
 
